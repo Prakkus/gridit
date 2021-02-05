35cd466693679e03f13d3d4daa693615ba9e6bae
Logan Stolmaker <logan@chromaplex.io>
2021-02-04 20:45:06 -0800

    moving undo/redo to buttons and file selection dialog

--- src/Persistence.js
+++ src/Persistence.js
@@ -1,4 +1,41 @@
-const Persistence = () => {
+const camelCaseToDash = (str) => {
+  return str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
+}
+
+const objectToStyleString = (obj) => {
+	let string = '';
+	Object.entries(obj).forEach(([key, value]) => {
+		string += `${camelCaseToDash(key)}: ${value}; `;
+	});
+	return string;
+}
+
+const downloadBlob = (blob) => {
+	const blobUrl = URL.createObjectURL(blob);
+
+	const anchor = document.createElement('a');
+	anchor.href = blobUrl;
+	anchor.target = "_blank";
+	anchor.download = `grid-${Date.now()}.json`;
+
+	// Auto click on a element, trigger the file download
+	anchor.click();
+
+	// This is required
+	URL.revokeObjectURL(blobUrl);
+}
+
+const Persistence = (dataResolver) => {
+
+	const svgStyle = {
+		fill: "#333",
+	}
+	const svgButtonStyle = {
+		width: "36px",
+		height: "36px",
+		borderRadius: '4px'
+	}
+
 	let currentlyLoadedJson = '';
 	let onLoadedFileChangedListeners = new Set();
 
@@ -9,6 +46,11 @@ const Persistence = () => {
 		onLoadedFileChangedListeners.forEach((listener) => listener(oldJson, newJson));
 	}
 
+	const loadJson = (jsonText) => {
+		const oldJson = currentlyLoadedJson;
+		currentlyLoadedJson = jsonText;
+		onLoadedFileTextChanged(oldJson, currentlyLoadedJson);
+	}
 
 	const handleFileDrop = (event) => {
 		event.preventDefault();
@@ -18,34 +60,59 @@ const Persistence = () => {
 		const droppedItem = event.dataTransfer.items[0];
 		if (droppedItem.kind !== 'file' || !droppedItem.type.match('^application/json')) return;
 		droppedItem.getAsFile().text().then(value => {
-			const oldJson = currentlyLoadedJson;
-			currentlyLoadedJson = value;
-			onLoadedFileTextChanged(oldJson, currentlyLoadedJson);
+			loadJson(value)
 		});
+	}
 
-		// Prevent default behavior (Prevent file from being opened)
+	const handleFileSelect = (event) => {
+		let file = event.target.files[0];
+		console.log(file.kind, file.type);
+		if (!file.type.match('^application/json')) return;
+		file.text().then(value => {
+			loadJson(value)
+		});
 	}
 
 	const GetCurrentlyLoadedJson = () => currentlyLoadedJson;
 
 	const handleFileDragOver = (event) => {
-		  console.log('File(s) in drop zone');
+		event.preventDefault();
+	}
 
-		  // Prevent default behavior (Prevent file from being opened)
-		  event.preventDefault();
+	const openFileSelectionWindow = () => {
+		let input = document.createElement('input');
+		input.type = 'file';
+		input.accept = 'application/json'
+
+		input.onchange = handleFileSelect;
+
+		input.click();
+	}
+
+	const downloadJsonSave = () => {
+		const jsonData = dataResolver();
+		const blob = new Blob([jsonData], {type : 'application/json'});
+		downloadBlob(blob);
 	}
 
 	const template = 
 	`
-		<div id="load-from-json-dropzone">
-				<p>Drag one or more files to this Drop Zone ...</p>
-		</div>
+	<div>
+		<button title="Load from JSON file" id="import-from-json" style="${objectToStyleString(svgButtonStyle)}">
+    		<img id="import-json-input" "${objectToStyleString(svgStyle)}" class="svg-icon" src="./src/icons/upload.svg" />
+    	</button>
+		<button title="Save as JSON file" id="download-as-json" style="${objectToStyleString(svgButtonStyle)}">
+    		<img class="svg-icon" src="./src/icons/download.svg" />
+    	</button>
+	</div>
 	`;
 
 	const loadFromJsonFieldElement = document.createElement('div')
 	loadFromJsonFieldElement.innerHTML = template;
-	loadFromJsonFieldElement.querySelector('#load-from-json-dropzone').addEventListener('dragover', handleFileDragOver);
-	loadFromJsonFieldElement.querySelector('#load-from-json-dropzone').addEventListener('drop', handleFileDrop);
+	loadFromJsonFieldElement.querySelector("#import-from-json").addEventListener('click', (e) => openFileSelectionWindow());
+	loadFromJsonFieldElement.querySelector("#import-from-json").addEventListener('dragover', handleFileDragOver);
+	loadFromJsonFieldElement.querySelector("#import-from-json").addEventListener('drop', handleFileDrop);
+	loadFromJsonFieldElement.querySelector('#download-as-json').addEventListener('click', (e) => downloadJsonSave());
 
 	return { loadFromJsonFieldElement, addOnLoadedFileChangedListener, GetCurrentlyLoadedJson };
 }
--- src/grid-utils.js
+++ src/grid-utils.js
@@ -1,21 +1,6 @@
-const downloadBlob = (blob) => {
-	const blobUrl = URL.createObjectURL(blob);
-
-	const anchor = document.createElement('a');
-	anchor.href = blobUrl;
-	anchor.target = "_blank";
-	anchor.download = `grid-${Date.now()}.json`;
-
-	// Auto click on a element, trigger the file download
-	anchor.click();
-
-	// This is required
-	URL.revokeObjectURL(blobUrl);
-}
-
 const isTargetACell = (event) => 'cellId' in event.target.dataset;
 
 //We only want to update the color if they are holding left click and dragging over a cell
 const isValidNodeColorDragEvent = (event) => event.button == 0 && isTargetACell(event);
 
-export { isValidNodeColorDragEvent, downloadBlob };
+export { isValidNodeColorDragEvent };
